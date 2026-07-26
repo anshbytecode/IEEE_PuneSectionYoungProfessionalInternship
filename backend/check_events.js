b389ccfb10ccdbdf2e44c49dd3db3a4b021c40ae
Anshul Bhilare <terminatoranshul@gmail.com>
2026-07-27 00:18:17 +0530

    Delete backend directory

--- backend/check_events.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const events = await prisma.event.findMany({});
-  console.log(JSON.stringify(events, null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
